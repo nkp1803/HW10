2f1dd81040fb151065615bef75c431cda0bd787a
konstantin <nkp.yalta@mail.ru>
2022-07-19 20:19:28 +0300

    exes2

---
+++ exes2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+System.Console.WriteLine("Введите число n =>");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число m =>");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int result = A(n, m);
+System.Console.WriteLine($"Функция Аккермана : {result} ");
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else
+    {
+        if ((n != 0) && (m == 0))
+        {
+            return A(n - 1, 1);
+        }
+        else
+        {
+            return A(n - 1, A(n, m - 1));
+        }
+    }
+}
+
