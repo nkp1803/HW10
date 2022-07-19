aa4b9a0d113888e1f02e5697dbbd4b8ba164d648
konstantin <nkp.yalta@mail.ru>
2022-07-19 19:44:46 +0300

    exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Есть монотонная последовательность, каждое число встречается 
+// ровно то количество раз, какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+// Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+// N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+
+System.Console.WriteLine("Введите число N =>");
+int count = Convert.ToInt32(Console.ReadLine());
+Recursion(count);
+void Recursion(int count, int number = 0, int sum = 0)
+{
+    if (count == 0)
+    {
+        return;
+    }
+    for (int i = 0; sum < count; i++)
+    {
+        sum += i; //sum = sum + i;
+        number = i;
+    }
+    Recursion(--count);
+    System.Console.Write(number + " ");
+}
